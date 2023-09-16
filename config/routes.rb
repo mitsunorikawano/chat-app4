427b12db3aa06cabab936ffdf4e27227c707e529
mitsunorikawano <mitsunori94@yahoo.co.jp>
2023-09-16 23:19:57 +0900

    ユーザー情報表示の実装2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  resources :users, only: [:edit, :update]
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
+  
 end
